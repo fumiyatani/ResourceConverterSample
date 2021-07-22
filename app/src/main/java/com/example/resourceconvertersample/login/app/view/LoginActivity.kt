2042e80fd2043ca326f1c4caa7cb1362cdfede8d
tanifumiya <fumiyadeveloper@gmail.com>
2021-07-23 00:06:55 +0900

    navigation componentを導入

--- app/src/main/java/com/example/resourceconvertersample/login/app/view/LoginActivity.kt
+++ app/src/main/java/com/example/resourceconvertersample/login/app/view/LoginActivity.kt
@@ -2,7 +2,7 @@ package com.example.resourceconvertersample.login.app.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
 import com.example.resourceconvertersample.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,12 +11,14 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-        supportFragmentManager.commit {
-            replace(
-                R.id.fragmentContainer,
-                LoginFragment.newInstance()
-            )
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return super.onNavigateUp()
     }
 }
